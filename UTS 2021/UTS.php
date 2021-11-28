510791ff38101db5e6864b0451f07328318be509
RifkiRamadhan27 <94811708+RifkiRamadhan27@users.noreply.github.com>
2021-11-28 16:01:23 +0700

    UTS PWB

---
+++ UTS 2021/UTS.php
@@ -0,0 +1,33 @@
+<html>
+<head><title>Latihan Dengan Objek Text</title></head>
+<body>
+
+<form>
+<H5> Form Pendaftaran</H5><hr>
+<pre>
+Nama: <input type="text" size="10" name="nama">
+<br>
+Lama: <input type="text" size="2" name="lama"> Hari
+<br>
+Kode Booking : <select name=kode_booking>
+ <option value="kode_booking">AL02102</option>
+ <option value="kode_booking">BG03025</option>
+ <option value="kode_booking">CR02111</option>
+ <option value="kode_booking">KM03075</option>
+ </select>
+ <br>
+ Jumlah: <input type="text" size="2" name="jumlah"> Orang
+<br>
+Jenis Pembayaran : <select name=jenis_pembayaran>
+ <option value="jenis_pembayaran">Kartu Kredit</option>
+ <option value="jenis_pembayaran">Debit</option>
+ </select>
+</pre>
+<br>
+
+<input type="button" value="Proses">
+<input type="reset" value="Hapus">
+</form>
+</body>
+</html>
+© 2021 UTS Mochamad Rifki Ramadhan - 202101087
